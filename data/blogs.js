9f91a4a10d8ac35c7acbccb0e5e7dcbc04b2a26a
Satyam Kumar Verman <satyamvarman2004@gmail.com>
2024-11-13 14:57:55 +0530

    added

---
+++ data/blogs.js
@@ -0,0 +1,16 @@
+blogs = [
+    {
+        title: "How to delete her photos ? ",
+        content: "Just format your smartphone - and remember no pain no gain",
+        slug: "delete-her-photots"
+    },
+    {
+        title: "How to stop stalking her profile on instagram?",
+        content: "Uninstall instagram for few weeks thats it",
+        slug: "stop-staking-her-profile"
+    },
+  
+]
+
+
+module.exports = blogs;
