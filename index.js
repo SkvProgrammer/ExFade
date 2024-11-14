9f130900ef4f603f8d9a3cab8935c232d45171f2
Satyam Kumar Verman <satyamvarman2004@gmail.com>
2024-11-14 10:10:43 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var exphbs  = require('express-handlebars');
 
 const path = require('path')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000 
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
